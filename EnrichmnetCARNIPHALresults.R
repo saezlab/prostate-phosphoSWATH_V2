c2a881295dcb2ad745f98ab252ba360567dc8c24
Alberto Valdeolivas <alvaldeolivas@gmail.com>
2020-02-12 17:55:39 +0100

    Script with the enrichment of Carniphal results

--- EnrichmnetCARNIPHALresults.R
+++ EnrichmnetCARNIPHALresults.R
@@ -1,5 +1,8 @@
+rm(list=ls());cat('\014');if(length(dev.list()>0)){dev.off()}
+
 library(readr)
 library(piano)
+library(dplyr)
 library(omicToolsTest)
 library(ggplot2)
 
@@ -24,7 +27,7 @@ sig_pathways <- runGSAhyper(sucesses, universe = bg, gsc = loadGSC(pathways))
 sig_pathways_df <- as.data.frame(sig_pathways$resTab)
 
 
-Kin_activity_PDTs <- as.data.frame(readRDS("Results/Kin_activity_PDTs.rds")) 
+Kin_activity_PDTs <- as.data.frame(readRDS("/home/alvaldeolivas/Documents/GitHub/Saezlab/prostate-phosphoSWATH_V2/Results/Kin_activity_PDTs.rds")) 
 
 Kin_activity_LNCaP_noInhib_t2_EGF <- Kin_activity_PDTs  %>%
     dplyr::select(LNCaP_noInhib_t2_EGF) 
@@ -43,10 +46,28 @@ PathwaysSign <- PathwaysSelect[PathwaysSelect$`Adjusted p-value` <= 0.00001,]
 PathwaysSign$pathway <- row.names(PathwaysSign) 
 PathwaysSign <- PathwaysSign[,c(4,1,2,3)]
 colnames(PathwaysSign) <- c("pathway","pvalue","AdjPvalu","sign")
+PathwaysSign$pathway <- as.factor(PathwaysSign$pathway )
 
 
-p <- ggplot(PathwaysSign, aes(x = pathway, y = -log10(pvalue))) + 
-    geom_bar(stat = "identity", alpha = PathwaysSign$sign) 
+Interesting_pathways <- c("KEGG_REGULATION_OF_ACTIN_CYTOSKELETON",
+                          "KEGG_PROSTATE_CANCER",
+                          "PID_AR_NONGENOMIC_PATHWAY")
+
+p <- ggplot(PathwaysSign, aes(x = reorder(pathway, pvalue), 
+                              y = -log10(pvalue) , fill=sign)) + 
+    geom_col() +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, 
+          # size = ifelse(PathwaysSign$pathway %in% Interesting_pathways, 8, 6), 
+          colour = ifelse(levels(reorder(PathwaysSign$pathway, PathwaysSign$pvalue)) %in% Interesting_pathways, "red", "grey40"),
+          face = ifelse(levels(reorder(PathwaysSign$pathway, PathwaysSign$pvalue)) %in% Interesting_pathways, "bold", "plain")),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank()) + 
+    xlab("")
+   #  scale_alpha(range = c(0.4, 0.8))
+    
+    #  geom_bar(stat = "identity", alpha = PathwaysSign$sign) + 
+    # t
     # theme(legend.position = "none",
     #       axis.title.x=element_blank(),
     #      axis.title.y=element_blank(),
@@ -54,6 +75,4 @@ p <- ggplot(PathwaysSign, aes(x = pathway, y = -log10(pvalue))) +
     #      axis.ticks.x=element_blank(),
     #      plot.margin = NULL) 
     # scale_y_continuous(expand = c(0,0))
-
-
 
